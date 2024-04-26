43967796162e3c0dc17e7256e49086dec547c15a
Ahayat10 <70821212+Ahayat10@users.noreply.github.com>
2024-04-25 22:37:34 -0400

    update
    fix

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/validate-user', (req, res) => {
+  const user = {
+    username: 'username',
+    password: 'password'
+  };
+
+  const { username, password } = req.query;
+
+  if (username === user.username && password === user.password) {
+    res.json({ success: true, message: 'User is valid' });
+  } else {
+    res.status(401).json({ success: false, message: 'Invalid credentials' });
+  }
+});
+
+app.get('/', (req, res) => {
+  
+    res.json({ success: true, message: 'insert /api/validate-user' });
+  
+  });
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
